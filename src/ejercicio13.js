0de97183e118f09bc57c6cf7a3d5d38fc25cf199
Sabrina@DESKTOP-K6VS1AG <sabrinapoma98@gmail.com>
2025-04-12 20:09:19 -0300

    Ejercicio 13 Resuelto por Sabrina Poma

--- src/ejercicio13.js
+++ src/ejercicio13.js
@@ -2,14 +2,16 @@
 //que reciba un arreglo numérico como parámetro e imprima cada elemento en una línea a parte. 
 
 
-
+//Recoremos el arreglo numerico
 const mostrarArreglo = (arreglo)=>{
     for (let i=0; i<arreglo.length; i++){
+        //mostramos por consola cada numero
         console.log(arreglo[i]);
     }
 };
 
  
-
+//Declaramos variable
 let numeros=[4,6,8,10];
+//llamamos a la funcion
 mostrarArreglo(numeros);
